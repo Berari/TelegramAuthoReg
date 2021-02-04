db39fc9a341711a96c1a919fed29a66407955eb8
Berari <72497359+Berari@users.noreply.github.com>
2021-02-04 14:46:22 +0700

    Add files via upload

--- proxy_parser.py
+++ proxy_parser.py
@@ -1,32 +1,15 @@
 import  requests
-import lxml
 
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-from fp.fp import FreeProxy
 
-def test():
-    proxies = {
-        'http': '182.53.50.184:8081',
-        #'https': '35.156.9.227:8080'
-    }
-
-    response = requests.get(url = 'https://2ip.ru/', proxies=proxies)
-
-    print(response)
-
-    proxy = FreeProxy(country_id=['RU', 'BR'], timeout=0.3, rand=True).get()
-    print(proxy)
-
-    re = requests.get(url = 'https://2ip.ru/', proxies=proxies)
-
-    print(re)
 
 class Parser:
     
-    def __init__(self, html):
+    def __init__(self):
         
-        self.html = html
+        self.html = requests.get('https://hidemy.name/ru/proxy-list/?type=hs#list', headers={'User-Agent': UserAgent().chrome})
+        self.proxy_list = []
 
     def parser(self):
 
@@ -39,24 +22,15 @@ class Parser:
                 i = i+1
 
             else:
+                proxy = []
                 data = block.find_all('td')
                 ip = str(data[0]).split('<td>')[1].split('</td>')[0]
                 port = str(data[1]).split('<td>')[1].split('</td>')[0]
-                print(ip)
-                print(port)
+                #print(ip)
+                #print(port)
 
+                proxy.append(ip)
+                proxy.append(port)
+                self.proxy_list.append(proxy)
 
-
-
-def main():
-    response = requests.get('https://hidemy.name/ru/proxy-list/?type=hs#list', headers={'User-Agent': UserAgent().chrome})
-
-    p = Parser(html = response.text)
-    p.parser()
-    test()
-    
-if __name__ == '__main__':
-    main()
-
-
-    
+        return self.proxy_list
