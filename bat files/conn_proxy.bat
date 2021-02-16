52aa1a9d8b4f95590e9bdfc0e9847c99d03da114
Berari <72497359+Berari@users.noreply.github.com>
2021-02-16 13:48:25 +0300

    Add files via upload

---
+++ bat files/conn_proxy.bat
@@ -0,0 +1,2 @@
+Set ApplicationPath="D:\Python Progs\TelegramAuthoReg\.reg files\connection_prox.reg"
+cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" %ApplicationPath%"
---
+++ bat files/off_proxy.bat
@@ -0,0 +1,2 @@
+Set ApplicationPath="D:\Python Progs\TelegramAuthoReg\.reg files\off_prox.reg"
+cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" %ApplicationPath%"
